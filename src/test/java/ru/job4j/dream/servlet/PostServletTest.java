cc57ddb6b0883cfae8d6341fe431ad64ab1fa545
c0dered273 <okaschuk@gmail.com>
2021-04-06 19:40:52 +0300

    Написать тест на PostServlet. [#458159]

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -1,12 +1,15 @@
 package ru.job4j.dream.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.PostsMemStore;
 import ru.job4j.dream.store.PostsPsqlStore;
 import ru.job4j.dream.store.Store;
@@ -14,6 +17,7 @@ import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 public class PostServletTest {
     @Test
@@ -28,6 +32,46 @@ public class PostServletTest {
             Mockito.when(mockReq.getParameter("name")).thenReturn(newUserName);
             new PostServlet().doPost(mockReq, mockResp);
             assertThat(memStore.findAll().iterator().next().getName(), is(newUserName));
+        }
+    }
+
+    @Test
+    public void whenEditPost() throws ServletException, IOException {
+        String page = "/post/edit.jsp";
+        HttpServletRequest reqMocked = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse respMocked = Mockito.mock(HttpServletResponse.class);
+        RequestDispatcher dispatcherMocked = Mockito.mock(RequestDispatcher.class);
+        HttpSession sessionMocked = Mockito.mock(HttpSession.class);
+        Mockito.when(reqMocked.getParameter("action")).thenReturn("edit");
+        Mockito.when(reqMocked.getRequestDispatcher(page)).thenReturn(dispatcherMocked);
+        Mockito.when(reqMocked.getSession()).thenReturn(sessionMocked);
+        Mockito.when(sessionMocked.getAttribute(Mockito.any(String.class))).thenReturn("");
+        new PostServlet().doGet(reqMocked, respMocked);
+        Mockito.verify(reqMocked, Mockito.times(1)).getRequestDispatcher(page);
+        Mockito.verify(dispatcherMocked).forward(reqMocked, respMocked);
+    }
+
+    @Test
+    public void whenDeletePost() throws ServletException, IOException {
+        try (MockedStatic<PostsPsqlStore> storeMocked = Mockito.mockStatic(PostsPsqlStore.class)) {
+            String page = "/posts.jsp";
+            Store<Post> memStore = PostsMemStore.instOf();
+            Post post = new Post(0, "NewPost", "Desc", "timestamp");
+            memStore.save(post);
+            storeMocked.when(PostsPsqlStore::instOf).thenReturn(memStore);
+            HttpServletRequest reqMocked = Mockito.mock(HttpServletRequest.class);
+            HttpServletResponse respMocked = Mockito.mock(HttpServletResponse.class);
+            RequestDispatcher dispatcherMocked = Mockito.mock(RequestDispatcher.class);
+            HttpSession sessionMocked = Mockito.mock(HttpSession.class);
+            Mockito.when(reqMocked.getParameter("id")).thenReturn(String.valueOf(post.getId()));
+            Mockito.when(reqMocked.getParameter("action")).thenReturn("delete");
+            Mockito.when(reqMocked.getRequestDispatcher(page)).thenReturn(dispatcherMocked);
+            Mockito.when(reqMocked.getSession()).thenReturn(sessionMocked);
+            Mockito.when(sessionMocked.getAttribute(Mockito.any(String.class))).thenReturn("");
+            new PostServlet().doGet(reqMocked, respMocked);
+            assertThat(memStore.findById(post.getId()), nullValue());
+            Mockito.verify(reqMocked, Mockito.times(1)).getRequestDispatcher(page);
+            Mockito.verify(dispatcherMocked).forward(reqMocked, respMocked);
         }
     }
 }
